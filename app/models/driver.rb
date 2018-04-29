8b314a3397092b9040c67ce08912238258a162c6
Esteban Lundin <esteban.lunding@utem.cl>
2018-04-28 23:32:18 -0300

    se añaden validaciones a drivers y trucks

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,12 @@
 class Driver < ApplicationRecord
     has_and_belongs_to_many :trucks
+
+    validates :first_name, presence: true {message: 'ingrese nombre'}
+    validates :last_name, presence: {message: 'ingrese apellido'}
+    validates :birthday, presence:{message: 'ingrese fecha de nacimiento'}
+    validates :income, presence: {message: 'ingrese sueldo'}, numericality: true
+    validates :license, presence: true
+    validates :rut, uniqueness: {message: 'rut ingresado no es valido'}
+
+
 end
--- app/models/truck.rb
+++ app/models/truck.rb
@@ -1,3 +1,9 @@
 class Truck < ApplicationRecord
     has_and_belongs_to_many :drivers
+
+    validates :name, presence: true
+    validates :load, presence: true
+    validates :number_plate, presence: true 
+    validates :detail
+
 end
