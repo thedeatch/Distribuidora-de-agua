113b108685c3f9b1f29b365912f2a2954fb82308
Esteban Lundin <esteban.lunding@utem.cl>
2018-04-30 00:06:17 -0300

    se han corregido algunos errores en las migraciones

--- db/migrate/20180430005044_drop_c_lients_table.rb
+++
@@ -1,5 +0,0 @@
-class DropCLientsTable < ActiveRecord::Migration[5.1]
-  def change
-      drop_table :c_lients
-  end
-end
--- db/migrate/20180430005305_drop_orders_table.rb
+++
@@ -1,5 +0,0 @@
-class DropOrdersTable < ActiveRecord::Migration[5.1]
-  def change
-      drop_table :orders
-  end
-end
