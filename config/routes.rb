3f3506be60060cabdd7d771e9b384b34d5592b15
Esteban Lundin <esteban.lunding@utem.cl>
2018-05-07 17:01:22 -0300

    se añaden las rutas para clientes
    Signed-off-by: Esteban Lundin <esteban.lunding@utem.cl>

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  #get 'welcome/index'
 
-  root 'welcome#index'
+  #root 'welcome#index'
 
-  resources :clients
+  get '/clients', to: 'clients#index', as: 'cliente'
+  get '/clients/new', to: 'clients#new'
+  get '/clients/:id', to: 'clients#show'
+  post '/clients', to: 'clients#create', as: 'crear_cliente'
+  get '/clients/:id/edit', to: 'clients#edit', as: 'editar_cliente'
+  put '/clients/:id', to: 'clients#update'
+  patch '/clients/:id', to: 'clients#update'
+  delete '/clients/:id', to: 'clients#destroy'
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
