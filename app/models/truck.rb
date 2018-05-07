94516b06f59bcd6c8c3a3726b3b13db987bd81f2
maximiliano diaz <maximilianodiazsandoval@gmail.com>
2018-05-07 12:35:36 -0700

    se sube validacion de patente

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -9,4 +9,18 @@ class Truck < ApplicationRecord
     validates :number_plate, presence: true
     validates :detail, length: { in: 7..254 }
 
+    p = "AAN212"
+
+  def nplate(string)
+    re = /[A-Z]{2}([A-Z]{2}|[0-9]{2})[0-9]{2}/
+    number_plate = re.match(string)
+    if number_plate
+      return true
+    else
+      return false
+    end
+  end
+
+  puts nplate(p)
+
 end
