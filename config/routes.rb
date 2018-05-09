9d0df487895c345ba28baf9a1e70b964ac2d1610
Esteban Lundin <esteban.lunding@utem.cl>
2018-05-09 15:29:35 -0300

    se crea la vista para clientes y para actualizar clientes
    Signed-off-by: Esteban Lundin <esteban.lunding@utem.cl>

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
 
   #root 'welcome#index'
 
-  get '/clients', to: 'clients#index', as: 'cliente'
+  get '/clients', to: 'clients#index', as: 'client'
   get '/clients/new', to: 'clients#new'
   get '/clients/:id', to: 'clients#show'
-  post '/clients', to: 'clients#create', as: 'crear_cliente'
-  get '/clients/:id/edit', to: 'clients#edit', as: 'editar_cliente'
+  post '/clients', to: 'clients#create', as: 'create_client'
+  get '/clients/:id/edit', to: 'clients#edit', as: 'edit_client'
   put '/clients/:id', to: 'clients#update'
   patch '/clients/:id', to: 'clients#update'
   delete '/clients/:id', to: 'clients#destroy'
