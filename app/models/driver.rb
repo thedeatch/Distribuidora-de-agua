a8f8d0b9b012c7fb05935ad71a63a00cb5dd653f
maximiliano <maximilianodiazsandoval@gmail.com>
2018-05-30 23:38:15 -0700

    se agregan validaciones para la fecha de nacimiento de los conductores

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -4,7 +4,7 @@ class Driver < ApplicationRecord
     VALID_NAME_REGEX = /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/
     validates :first_name, presence: {message: 'ingrese nombre'}, length: { in: 3..50 }, format: {with: VALID_NAME_REGEX}
     validates :last_name, presence: {message: 'ingrese apellido'}, length: { in: 3..50 }, format: {with: VALID_NAME_REGEX}
-    validate :fecha_nacimiento_futuro
+    validate :fecha_nacimiento_futuro, :mayor_de_edad
     validates :income, presence: {message: 'ingrese sueldo'}, numericality: { only_integer: true }
     validates :license, inclusion: { in: [ true, false ] }
     validates :rut, rut: true, uniqueness: true, presence: true
@@ -15,4 +15,16 @@ class Driver < ApplicationRecord
             errors.add(:birthday, "Fecha de nacimiento incorrecta, el conductor no pudo haber nacido hoy o en el futuro")
         end
     end
+
+    def age
+      now = Time.now.utc.to_date
+      now.year - birthday.year - (birthday.to_date.change(:year => now.year) > now ? 1 : 0)
+    end
+
+    def mayor_de_edad
+      if age < 18
+        errors.add(:birthday, "Fecha de nacimiento incorrecta, el conductor no puede ser menor de edad")
+      end
+    end
+
 end
