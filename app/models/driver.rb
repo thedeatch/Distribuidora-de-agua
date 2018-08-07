22931405bac48e2f6386653e962defbcdc424942
maximiliano <maximilianodiazsandoval@gmail.com>
2018-08-06 21:05:05 -0700

    Se soluciona error de que no acepta sueldo vacio,tambien se arregla eliminar en el controlador de conductores

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,10 +2,10 @@ class Driver < ApplicationRecord
     has_and_belongs_to_many :trucks
 
     VALID_NAME_REGEX = /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/
-    validates :first_name, presence: {message: 'ingrese nombre'}, length: { in: 3..50 }, format: {with: VALID_NAME_REGEX}
-    validates :last_name, presence: {message: 'ingrese apellido'}, length: { in: 3..50 }, format: {with: VALID_NAME_REGEX}
+    validates :first_name, presence: {message: 'ingrese nombre'}, length: { in: 2..50 }, format: {with: VALID_NAME_REGEX}
+    validates :last_name, presence: {message: 'ingrese apellido'}, length: { in: 2..50 }, format: {with: VALID_NAME_REGEX}
     validate :fecha_nacimiento_futuro, :mayor_de_edad
-    validates :income, :incomemin, presence: {message: 'ingrese sueldo'}, numericality: { only_integer: true }
+    validates :income, presence: {message: 'ingrese sueldo'}, numericality: {only_integer: true, greater_than_or_equal_to: 5000}
     validates :license, inclusion: { in: [ true, false ] }
     validates :rut, rut: true, uniqueness: true, presence: true
     #validates_with RutValidator
@@ -31,9 +31,10 @@ class Driver < ApplicationRecord
     [first_name, last_name].compact.join(' ')
   end
 
+
+   #def incomemin
+     #if income.blank? && income < 5000
+      # errors.add(:income, "El valor del sueldo debe ser superior a 5000")
+     #end
+  #end
 end
-   def incomemin
-     if income < 5000
-       errors.add(:income, "El valor del sueldo debe ser superior a 5000")
-     end
-  end
--- app/models/truck.rb
+++ app/models/truck.rb
@@ -3,8 +3,8 @@ class Truck < ApplicationRecord
     has_many :products, through: :orders
     has_many :clients, through: :orders
     has_and_belongs_to_many :drivers
-    VALID_NUMBERPLATE_REGEX = /[A-Z]{2}([A-Z]{2}|[0-9]{2})[0-9]{2}/
-    validates :name, presence: true, length: { in: 8..80 }
+    VALID_NUMBERPLATE_REGEX = /[A-Z]{2}([A-Z]{2}|[0-9]{2})([0-9]{2})/
+    validates :name, presence: true, length: { in: 3..80 }
     validates :load, presence: true, numericality: :true
     validates :number_plate, presence: true, length: { in: 6..6 }, format: {with: VALID_NUMBERPLATE_REGEX}
     validates :detail, length: { in: 4..254 }
