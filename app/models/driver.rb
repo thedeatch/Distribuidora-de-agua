aa2e21dea9680a0ed276aeac59f1d21254086660
maximiliano <maximilianodiazsandoval@gmail.com>
2018-08-09 21:45:35 -0700

    Se implementa una mejor lectura para los errores

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,7 +2,7 @@ class Driver < ApplicationRecord
     has_and_belongs_to_many :trucks
 
     VALID_NAME_REGEX = /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/
-    validates :first_name, presence: {message: 'ingrese nombre'}, length: { in: 2..50 }, format: {with: VALID_NAME_REGEX}
+    validates :first_name, length: { in: 2..50 , :message => "^ El nombre tiene que estar entre 2 a 50 caracteres"}, format: { with: VALID_NAME_REGEX , :message => "El nombre no tiene formato valido"}
     validates :last_name, presence: {message: 'ingrese apellido'}, length: { in: 2..50 }, format: {with: VALID_NAME_REGEX}
     validate :fecha_nacimiento_futuro, :mayor_de_edad
     validates :income, presence: {message: 'ingrese sueldo'}, numericality: {only_integer: true, greater_than_or_equal_to: 5000}
