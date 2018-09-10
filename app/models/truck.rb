f1fc1c00d952987dc86afe89e6eda54e07933324
maximiliano <maximilianodiazsandoval@gmail.com>
2018-09-10 16:12:04 -0700

    se implementa mejoras de lectura en los errores de camion

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -7,7 +7,7 @@ class Truck < ApplicationRecord
 
 
     VALID_NUMBERPLATE_REGEX = /[A-Z]{2}([A-Z]|[0-9]){2}[0-9]{2}/i
-    validates :name, presence: { message: "no puede estar en blanco" }, length: { in: 3..80 , :message => "La marca debe ser superior a 3 caracteres"}
+    validates :name, presence: { message: "no puede estar en blanco" }, length: { in: 3..80 , :message => "La marca debe ser a lo menos de 3 caracteres"}
     validates :load, presence: { message: "no puede estar en blanco" }, numericality: { message: "no es un numero" }
     validates :number_plate, presence: { message: "no puede estar en blanco" }, length: { in: 6..6 , :message => "La patente debe tener 6 caracteres"}, format: {with: VALID_NUMBERPLATE_REGEX , message: "es invalido" }
     before_save :uppercase_numberplate
